5b953b838e20d52d2f9c0565b5b873abf44bab8c
Marco Arias <marcoarias@outlook.com>
2024-06-06 15:31:16 -0500

    repo init

---
+++ src/components/services.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+export const Services = (props) => {
+  return (
+    <div id="services" className="text-center">
+      <div className="container">
+        <div className="section-title">
+          <h2>Nuestros Servicios</h2>
+          <hr className="title-line"/>
+          {/* <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit duis sed
+            dapibus leonec.
+          </p> */}
+        </div>
+        <div className="row">
+          {props.data
+            ? props.data.map((d, i) => (
+                <div key={`${d.name}-${i}`} className="col-md-4 frame">
+                  {" "}
+                  {/* <i className={d.icon}></i> */}
+                  <img src={d.imageSrc} alt="service" width="275" height="auto" className="service-img"/>
+                  <div className="service-desc">
+                    <h3>{d.name}</h3>
+                    {/* <p>{d.text}</p> */}
+                  </div>
+                  <hr className="title-line"/>
+                </div>
+              ))
+            : "loading"}
+        </div>
+      </div>
+    </div>
+  );
+};
